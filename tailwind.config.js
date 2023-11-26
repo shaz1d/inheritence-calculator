62bb254bed13d2e7046fdd6f9f0bdf4988456787
Shazid <shazid.dev@gmail.com>
2023-11-26 16:09:12 +0600

    ci: add custom font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(172, 67%, 45%)",
+        pest: {
+          200: "hsl(189, 41%, 97%)",
+          300: "hsl(185, 41%, 84%)",
+          400: "hsl(184, 14%, 56%)",
+          450: "hsl(173, 61%, 77%)",
+          500: "hsl(186, 14%, 43%)",
+          550: "hsl(172, 69%, 45%)",
+          600: "hsl(183, 100%, 15%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
