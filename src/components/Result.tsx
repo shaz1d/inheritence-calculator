e2a3565fcae1e746d5cd9450474aa8127feab053
Shazid <shazid.dev@gmail.com>
2023-11-27 11:10:25 +0600

    feat: result component add

---
+++ src/components/Result.tsx
@@ -0,0 +1,18 @@
+import SingleResult from "./SingleResult";
+
+type Props = {
+  result?: object;
+};
+
+const Result = ({ result }: Props) => {
+  return (
+    <div className="bg-pest-600 p-4 rounded-3xl text-white">
+      <h2 className="text-xl">Result</h2>
+      <div className="grid grid-cols-2 gap-5">
+        <SingleResult />
+      </div>
+    </div>
+  );
+};
+
+export default Result;
