8ee6fbab0b386bba9b8b9a84e4add1ab49b33ce4
Shazid <shazid.dev@gmail.com>
2023-11-27 19:58:05 +0600

    feat: number of son, daughter and wife state add

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -17,6 +17,14 @@ const MainContent = () => {
     hasHusband: false,
   });
 
+  const [numberOfSons, setNumberOfSons] = useState(conditions.hasSon ? 1 : 0);
+  const [numberOfDaughters, setNumberOfDaughters] = useState(
+    conditions.hasDaughter ? 1 : 0
+  );
+  const [numberOfWives, setNumberOfWives] = useState(
+    conditions.hasWife ? 1 : 0
+  );
+
   const [state, dispatch] = useReducer(reducer, conditions);
 
   function calculate() {
