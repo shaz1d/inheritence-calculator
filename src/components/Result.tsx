6b8b0d4746e09829e292c43acdbb92a74fb969e4
Shazid <shazid.dev@gmail.com>
2023-11-27 11:25:06 +0600

    feat: single result component add

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Result = ({ result }: Props) => {
   return (
-    <div className="bg-pest-600 p-4 rounded-3xl text-white">
+    <div className="bg-pest-600 p-8 rounded-3xl text-white">
       <h2 className="text-xl">Result</h2>
       <div className="grid grid-cols-2 gap-5">
         <SingleResult />
---
+++ src/components/SingleResult.tsx
@@ -0,0 +1,25 @@
+type Props = {};
+
+const SingleResult = (props: Props) => {
+  return (
+    <div className="p-5 bg-pest-700">
+      <div className="w-full flex justify-between items-center text-lg mb-4">
+        <span>Father Gets</span> <span className="text-right">0.123%</span>
+      </div>
+      <div className="w-full flex justify-between items-center text-base font-normal py-1">
+        <span>Land</span> <span className="text-right">0.123% decimal</span>
+      </div>
+      <div className="w-full flex justify-between items-center text-base font-normal py-1">
+        <span>Gold</span> <span className="text-right">0.123% vori</span>
+      </div>
+      <div className="w-full flex justify-between items-center text-base font-normal py-1">
+        <span>Silver</span> <span className="text-right">0.123% vori</span>
+      </div>
+      <div className="w-full flex justify-between items-center text-base font-normal py-1">
+        <span>Currency</span> <span className="text-right">0.123% taka</span>
+      </div>
+    </div>
+  );
+};
+
+export default SingleResult;
