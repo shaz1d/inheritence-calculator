185950787ead7e281130cd4c37a33191faf4cb7a
Shazid <shazid.dev@gmail.com>
2023-11-27 19:09:13 +0600

    feat: relatives input check validation add

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,22 +1,145 @@
+import { useReducer, useState } from "react";
 import PropertyInput from "./PropertyInput";
 import RelativeInput from "./RelativeInput";
 import Result from "./Result";
 
+type State = {
+  hasChild: boolean;
+  hasSon: boolean;
+  hasDaughter: boolean;
+  hasFather: boolean;
+  hasMother: boolean;
+  hasTrueGrandfather: boolean;
+  hasTrueGrandmother: boolean;
+  hasWife: boolean;
+  hasHusband: boolean;
+};
+type Action = {
+  type: string;
+};
+
 const MainContent = () => {
+  const [conditions, setConditions] = useState({
+    hasChild: false,
+    hasSon: false,
+    hasDaughter: false,
+    hasFather: false,
+    hasMother: false,
+    hasTrueGrandfather: false,
+    hasTrueGrandmother: false,
+    hasWife: false,
+    hasHusband: false,
+  });
+
+  function reducer(state: State, action: Action) {
+    switch (action.type) {
+      case "husband": {
+        return {
+          ...state,
+          hasHusband: state.hasWife === true ? false : !state.hasHusband,
+        };
+      }
+      case "wife": {
+        return {
+          ...state,
+          hasWife: state.hasHusband === true ? false : !state.hasWife,
+        };
+      }
+      case "son": {
+        return {
+          ...state,
+          hasSon: !state.hasSon,
+          hasChild: state.hasSon || state.hasDaughter ? true : false,
+        };
+      }
+      case "daughter": {
+        return {
+          ...state,
+          hasDaughter: !state.hasDaughter,
+          hasChild: state.hasSon || state.hasDaughter ? true : false,
+        };
+      }
+      case "father": {
+        return {
+          ...state,
+          hasFather: !state.hasFather,
+        };
+      }
+      case "mother": {
+        return {
+          ...state,
+          hasMother: !state.hasMother,
+        };
+      }
+      case "grandfather": {
+        return {
+          ...state,
+          hasTrueGrandfather: !state.hasTrueGrandfather,
+        };
+      }
+      case "grandmother": {
+        return {
+          ...state,
+          hasTrueGrandmother: !state.hasTrueGrandmother,
+        };
+      }
+      default:
+        return state;
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, conditions);
+
+  function calculate() {
+    setConditions({ ...conditions, hasHusband: true });
+    console.log(conditions);
+  }
+
   return (
     <div>
       <div className="bg-white p-4 rounded-3xl grid grid-cols-8 gap-4 mb-6">
         <div className="col-span-4 p-6 text-pest-600">
           <h2 className="text-xl">Relatives List</h2>
           <div className="grid grid-cols-2 gap-5 mt-6">
-            <RelativeInput name="Husband" />
-            <RelativeInput name="Wife" />
-            <RelativeInput name="Son" />
-            <RelativeInput name="Daughter" />
-            <RelativeInput name="Father" />
-            <RelativeInput name="Mother" />
-            <RelativeInput name="Grand Father" />
-            <RelativeInput name="Grand Mother" />
+            <RelativeInput
+              name="Husband"
+              isChecked={state.hasHusband}
+              dispatch={dispatch}
+            />
+            <RelativeInput
+              name="Wife"
+              isChecked={state.hasWife}
+              dispatch={dispatch}
+            />
+            <RelativeInput
+              name="Son"
+              isChecked={state.hasSon}
+              dispatch={dispatch}
+            />
+            <RelativeInput
+              name="Daughter"
+              isChecked={state.hasDaughter}
+              dispatch={dispatch}
+            />
+            <RelativeInput
+              name="Father"
+              isChecked={state.hasFather}
+              dispatch={dispatch}
+            />
+            <RelativeInput
+              name="Mother"
+              isChecked={state.hasMother}
+              dispatch={dispatch}
+            />
+            <RelativeInput
+              name="Grandfather"
+              isChecked={state.hasTrueGrandfather}
+              dispatch={dispatch}
+            />
+            <RelativeInput
+              name="Grandmother"
+              isChecked={state.hasTrueGrandmother}
+              dispatch={dispatch}
+            />
           </div>
         </div>
         <div className="col-span-4 bg-pest-200 rounded-2xl p-5 text-pest-600">
@@ -26,7 +149,10 @@ const MainContent = () => {
             <PropertyInput name="Gold" type="vori" />
             <PropertyInput name="Silver" type="vori" />
             <PropertyInput name="Currency" type="taka" />
-            <button className="col-span-2 rounded-md bg-pest-600 text-white py-3 text-xl">
+            <button
+              className="col-span-2 rounded-md bg-pest-600 text-white py-3 text-xl"
+              onClick={calculate}
+            >
               Calculate
             </button>
           </div>
--- src/components/RelativeInput.tsx
+++ src/components/RelativeInput.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
+type Action = {
+  type: string;
+};
 type Props = {
   name: string;
+  isChecked: boolean;
+  dispatch: React.Dispatch<Action>;
 };
 
-const RelativeInput = ({ name }: Props) => {
-  const [isChecked, setIsChecked] = useState(false);
+const RelativeInput = ({ name, isChecked, dispatch }: Props) => {
   const [sons, setSons] = useState(1);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,7 +26,9 @@ const RelativeInput = ({ name }: Props) => {
         className="cursor-pointer accent-pest-600"
         type="checkbox"
         checked={isChecked}
-        onChange={() => setIsChecked(!isChecked)}
+        onChange={() => {
+          dispatch({ type: name.toLowerCase() });
+        }}
         name={name}
         id={name}
       />
@@ -30,8 +36,8 @@ const RelativeInput = ({ name }: Props) => {
       {name.toLowerCase() === "husband" ||
       name.toLowerCase() === "father" ||
       name.toLowerCase() === "mother" ||
-      name.toLowerCase() === "grand father" ||
-      name.toLowerCase() === "grand mother" ||
+      name.toLowerCase() === "grandfather" ||
+      name.toLowerCase() === "grandmother" ||
       !isChecked ? null : (
         <input
           className="bg-pest-200 h-10 w-10 rounded-md text-center accent-pest-600"
