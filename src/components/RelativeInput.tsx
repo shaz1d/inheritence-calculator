489fc1577b103d9066f8c26959a793d204203e88
Shazid <shazid.dev@gmail.com>
2023-11-28 13:12:31 +0600

    fix: relative with multiple value validation add

--- src/components/RelativeInput.tsx
+++ src/components/RelativeInput.tsx
@@ -16,6 +16,44 @@ const RelativeInput = ({
   state,
   setState,
 }: Props) => {
+  const maxValue = name.toLowerCase() === "wife" ? 4 : 20;
+  const minValue = 1;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(e.target.value);
+
+    if (setState) {
+      if (newValue > maxValue) {
+        setState(maxValue);
+      } else if (newValue < minValue) {
+        setState(minValue);
+      } else {
+        setState(newValue);
+      }
+    }
+  };
+
+  const increment = () => {
+    if (setState) {
+      setState((prev) => {
+        if (prev < maxValue) {
+          return prev + 1;
+        } else {
+          return prev;
+        }
+      });
+    }
+  };
+  const decrement = () => {
+    if (setState) {
+      setState((prev) => {
+        if (prev > minValue) {
+          return prev - 1;
+        } else {
+          return prev;
+        }
+      });
+    }
+  };
   return (
     <div className="flex gap-2 items-center min-h-[40px]">
       <input
@@ -37,20 +75,30 @@ const RelativeInput = ({
       name.toLowerCase() === "grandfather" ||
       name.toLowerCase() === "grandmother" ||
       !isChecked ? null : (
-        <input
-          className="bg-pest-200 h-10 w-14 rounded-md text-center accent-pest-600"
-          type="number"
-          name=""
-          id=""
-          max={name.toLowerCase() === "wife" ? 4 : 20}
-          min={1}
-          value={state}
-          onChange={(e) => {
-            if (setState) {
-              setState(parseInt(e.target.value, 10));
-            }
-          }}
-        />
+        <div className="flex items-center">
+          <button
+            className="text-lg h-5 w-5  text-white flex justify-center items-center bg-pest-600 rounded-sm z-10"
+            onClick={decrement}
+          >
+            -
+          </button>
+          <input
+            className="bg-pest-200 h-10 w-10 rounded-md text-center accent-pest-600"
+            type="number"
+            name=""
+            id=""
+            max={name.toLowerCase() === "wife" ? 4 : 20}
+            min={1}
+            value={state}
+            onChange={handleChange}
+          />
+          <button
+            className="text-lg h-5 w-5 text-white flex justify-center items-center bg-pest-600 rounded-sm"
+            onClick={increment}
+          >
+            +
+          </button>
+        </div>
       )}
     </div>
   );
